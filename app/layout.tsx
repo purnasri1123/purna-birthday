5297b1459ccf10dcbabd8657fe4958096c7aa557
purnasri1123 <purnasri1123@gmail.com>
2026-09-22 22:38:12 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Bangaram 🩷💙",
+  title: "Purna | 23 September",
   description: "A little birthday surprise...",
 };
 
